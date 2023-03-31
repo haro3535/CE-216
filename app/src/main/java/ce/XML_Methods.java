5d1d711a36ab02132ce7090090ca03c5f2f24c6a
Oğuz Kaan Tavur <oguztavur@gmail.com>
2023-03-31 22:36:37 +0300

    Some methods for XML files

--- app/src/main/java/ce/XML_Methods.java
+++ app/src/main/java/ce/XML_Methods.java
@@ -1,6 +1,9 @@
 package ce;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -10,6 +13,46 @@ import java.io.File;
 import java.io.IOException;
 
 public class XML_Methods implements Runnable {
+
+
+    public static void SearchMeaning(String word) throws ParserConfigurationException, IOException, SAXException {
+        File file = new File("eng-tur.xml");
+        DocumentBuilderFactory fac = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = fac.newDocumentBuilder();
+        Document doc = builder.parse(file);
+        doc.getDocumentElement().normalize();
+        NodeList nodeL = doc.getElementsByTagName("entry");
+        for (int i=0;i<nodeL.getLength();i++){
+            Element element = (Element) nodeL.item(i);
+            if (element.getElementsByTagName("orth").item(0).getTextContent().equals(word)){
+                Node node = element.getElementsByTagName("orth").item(0);
+                NodeList nodeL2 = node.getParentNode().getParentNode().getChildNodes();
+                for (int j=3;j<nodeL2.getLength();j++){
+                    if(nodeL2.item(j).getNodeName()=="sense"){
+                        System.out.println("Meaning " + (j/2) + ": " + nodeL2.item(j).getChildNodes().item(1).getChildNodes().item(1).getTextContent());
+                    }
+                }
+            }
+        }
+    }
+
+    public static void FindWord(String word) throws ParserConfigurationException, IOException, SAXException {
+
+        File file = new File("eng-tur.xml");
+        DocumentBuilderFactory fac = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = fac.newDocumentBuilder();
+        Document doc = builder.parse(file);
+        doc.getDocumentElement().normalize();
+        NodeList nodeL = doc.getElementsByTagName("entry");
+        for (int i=0;i<nodeL.getLength();i++){
+            Element element = (Element) nodeL.item(i);
+            if (element.getElementsByTagName("orth").item(0).getTextContent().equals(word)){
+                System.out.println("Word found!");
+                System.out.println(word);
+                System.out.println(element.getElementsByTagName("orth").item(0).getTextContent());
+            }
+        }
+    }
 
     protected void search(String filepath){
 
