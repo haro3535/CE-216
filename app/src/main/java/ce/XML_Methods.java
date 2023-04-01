84e3cd2741b9a2f527189e201cf3b288bff72fc3
haro3535 <66617253+haro3535@users.noreply.github.com>
2023-04-01 13:46:35 +0300

    Some XMl methods updated

--- app/src/main/java/ce/XML_Methods.java
+++ app/src/main/java/ce/XML_Methods.java
@@ -11,33 +11,62 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class XML_Methods implements Runnable {
 
+    private final ArrayList<Node> foundedEntries = new ArrayList<>();
 
-    public static void SearchMeaning(String word) throws ParserConfigurationException, IOException, SAXException {
-        File file = new File("eng-tur.xml");
+
+    public void searchMeaning(String word){
+        /*File file = new File("eng-tur.xml");
         DocumentBuilderFactory fac = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = fac.newDocumentBuilder();
         Document doc = builder.parse(file);
         doc.getDocumentElement().normalize();
         NodeList nodeL = doc.getElementsByTagName("entry");
         for (int i=0;i<nodeL.getLength();i++){
-            Element element = (Element) nodeL.item(i);
-            if (element.getElementsByTagName("orth").item(0).getTextContent().equals(word)){
-                Node node = element.getElementsByTagName("orth").item(0);
-                NodeList nodeL2 = node.getParentNode().getParentNode().getChildNodes();
-                for (int j=3;j<nodeL2.getLength();j++){
-                    if(Objects.equals(nodeL2.item(j).getNodeName(), "sense")){
-                        System.out.println("Meaning " + (j/2) + ": " + nodeL2.item(j).getChildNodes().item(1).getChildNodes().item(1).getTextContent());
+                Element element = (Element) nodeL.item(i);
+                if (element.getElementsByTagName("orth").item(0).getTextContent().equals(word)){
+                    Node node = element.getElementsByTagName("orth").item(0);
+                    NodeList nodeL2 = node.getParentNode().getParentNode().getChildNodes();
+                    for (int j=3;j<nodeL2.getLength();j++){
+                        if(Objects.equals(nodeL2.item(j).getNodeName(), "sense")){
+                            System.out.println("Meaning " + (j/2) + ": " + nodeL2.item(j).getChildNodes().item(1).getChildNodes().item(1).getTextContent());
+                        }
+                    }
+                }
+            }
+         */
+
+        for (Node foundedEntry : foundedEntries) {
+
+            Element entry = (Element) foundedEntry;
+
+            // Bazı XML dosyaları birden fazla sense tagına sahip o yüzden eğer böyle bir durumla karşılaşırsa sıkıntı çıkmasın diye
+            NodeList senses = entry.getElementsByTagName("sense");
+
+            for (int j = 0; j < senses.getLength(); j++) {
+
+                Element currentSense = (Element) senses.item(j);
+
+                NodeList cites = currentSense.getElementsByTagName("cit");
+
+                for (int k = 0; k < cites.getLength(); k++) {
+                    Element citeT = (Element) cites.item(k);
+
+                    int meaningIndex = 1;
+                    if (citeT.getAttribute("type").equals("trans")) {
+                        String quote = citeT.getElementsByTagName("quote").item(0).getTextContent();
+                        System.out.println("Meaning " + (meaningIndex) + ": " + quote);
                     }
                 }
             }
         }
     }
 
-    public static void FindWord(String word) throws ParserConfigurationException, IOException, SAXException {
+    public void findWord(String word) throws ParserConfigurationException, IOException, SAXException {
 
         File file = new File("eng-tur.xml");
         DocumentBuilderFactory fac = DocumentBuilderFactory.newInstance();
@@ -49,6 +78,9 @@ public class XML_Methods implements Runnable {
             Element element = (Element) nodeL.item(i);
             if (element.getElementsByTagName("orth").item(0).getTextContent().equals(word)){
                 System.out.println("Word found!");
+
+                // Sonradan tekrar dosyayı aratmamak için bulunan entry i kayıt ediyor
+                foundedEntries.add(nodeL.item(i));
             }
         }
     }
