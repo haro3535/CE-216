bd46f25ebcc56e7825a5885cbc149f64bd8391b9
Boztepe12 <bztpali02@gmail.com>
2023-04-06 08:51:43 +0300

    for against if

--- app/src/main/java/ce/GUI_Actions.java
+++ app/src/main/java/ce/GUI_Actions.java
@@ -148,7 +148,7 @@ public class GUI_Actions {
 
         }
 
-        int meaningNumber = 1;
+        int meaningNumber = 4;
         BorderPane borderPane = new BorderPane();
 
         MenuBar mainMenuBar = new MenuBar();
@@ -177,319 +177,27 @@ public class GUI_Actions {
         HBox.setMargin(searchButton, new Insets(0,40,0,40));
         searchButton.setOnAction(event -> searchingAction(searchingText,stage, scene));
 
+        searchMeaningBox.getChildren().add(textWithButton);
 
-        if (meaningNumber==1) {
-            HBox meaningButtonBox1 = new HBox();
 
-            TextArea meaning1 = new TextArea(value);
-            meaning1.setEditable(false);
-            HBox.setMargin(meaning1, new Insets(0, 20, 80, 60));
-            HBox.setHgrow(meaning1,Priority.ALWAYS);
-            meaning1.setPrefHeight(80);
-            meaning1.setPrefWidth(350);
+        for (int i = 0; i < meaningNumber; i++) {
+            HBox meaningButtonBox = new HBox();
 
-            Button meaning1Button = new Button("<--");
-            HBox.setMargin(meaning1Button, new Insets(25, 10, 0, 0));
-            meaning1Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
+            TextArea meaning = new TextArea(value);
+            meaning.setEditable(false);
+            HBox.setMargin(meaning, new Insets(0, 20, 0, 60));
+            HBox.setHgrow(meaning,Priority.ALWAYS);
+            meaning.setPrefHeight(80);
+            meaning.setPrefWidth(350);
 
-            meaningButtonBox1.getChildren().addAll(meaning1, meaning1Button);
+            Button meaningButton = new Button("<--");
+            HBox.setMargin(meaningButton, new Insets(25, 10, 0, 0));
+            meaningButton.setOnAction(event -> choosingLanguage(textField,stage, scene));
 
-            searchMeaningBox.getChildren().addAll(textWithButton, meaningButtonBox1);
-        }
-        if (meaningNumber==2) {
-            HBox meaningButtonBox1 = new HBox();
+            meaningButtonBox.getChildren().addAll(meaning, meaningButton);
 
-            TextArea meaning1 = new TextArea(language1);
-            HBox.setMargin(meaning1, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning1,Priority.ALWAYS);
-            meaning1.setPrefHeight(80);
-            meaning1.setPrefWidth(350);
+            searchMeaningBox.getChildren().add( meaningButtonBox);
 
-            Button meaning1Button = new Button("<--");
-            HBox.setMargin(meaning1Button, new Insets(25, 10, 0, 0));
-            meaning1Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox1.getChildren().addAll(meaning1, meaning1Button);
-
-            HBox meaningButtonBox2 = new HBox();
-
-            TextArea meaning2 = new TextArea(language1);
-            HBox.setMargin(meaning2, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning2,Priority.ALWAYS);
-            meaning2.setPrefHeight(80);
-            meaning2.setPrefWidth(350);
-
-            Button meaning2Button = new Button("<--");
-            HBox.setMargin(meaning2Button, new Insets(25, 10, 0, 0));
-            meaning2Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox2.getChildren().addAll(meaning2, meaning2Button);
-
-            searchMeaningBox.getChildren().addAll(textWithButton, meaningButtonBox1, meaningButtonBox2);
-        }if (meaningNumber==3) {
-            HBox meaningButtonBox1 = new HBox();
-
-            TextArea meaning1 = new TextArea(language1);
-            HBox.setMargin(meaning1, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning1,Priority.ALWAYS);
-            meaning1.setPrefHeight(80);
-            meaning1.setPrefWidth(350);
-
-            Button meaning1Button = new Button("<--");
-            HBox.setMargin(meaning1Button, new Insets(25, 10, 0, 0));
-            meaning1Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox1.getChildren().addAll(meaning1, meaning1Button);
-
-            HBox meaningButtonBox2 = new HBox();
-
-            TextArea meaning2 = new TextArea(language1);
-            HBox.setMargin(meaning2, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning2,Priority.ALWAYS);
-            meaning2.setPrefHeight(80);
-            meaning2.setPrefWidth(350);
-
-            Button meaning2Button = new Button("<--");
-            HBox.setMargin(meaning2Button, new Insets(25, 10, 0, 0));
-            meaning2Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox2.getChildren().addAll(meaning2, meaning2Button);
-
-            HBox meaningButtonBox3 = new HBox();
-
-            TextArea meaning3 = new TextArea(language1);
-            HBox.setMargin(meaning3, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning3,Priority.ALWAYS);
-            meaning3.setPrefHeight(80);
-            meaning3.setPrefWidth(350);
-
-            Button meaning3Button = new Button("<--");
-            HBox.setMargin(meaning3Button, new Insets(25, 10, 0, 0));
-            meaning3Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox3.getChildren().addAll(meaning3, meaning3Button);
-
-            searchMeaningBox.getChildren().addAll(textWithButton, meaningButtonBox1, meaningButtonBox2, meaningButtonBox3);
-        }if (meaningNumber==4) {
-            HBox meaningButtonBox1 = new HBox();
-
-            TextArea meaning1 = new TextArea(language1);
-            HBox.setMargin(meaning1, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning1,Priority.ALWAYS);
-            meaning1.setPrefHeight(80);
-            meaning1.setPrefWidth(350);
-
-            Button meaning1Button = new Button("<--");
-            HBox.setMargin(meaning1Button, new Insets(25, 10, 0, 0));
-            meaning1Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox1.getChildren().addAll(meaning1, meaning1Button);
-
-            HBox meaningButtonBox2 = new HBox();
-
-            TextArea meaning2 = new TextArea(language1);
-            HBox.setMargin(meaning2, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning2,Priority.ALWAYS);
-            meaning2.setPrefHeight(80);
-            meaning2.setPrefWidth(350);
-
-            Button meaning2Button = new Button("<--");
-            HBox.setMargin(meaning2Button, new Insets(25, 10, 0, 0));
-            meaning2Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox2.getChildren().addAll(meaning2, meaning2Button);
-
-            HBox meaningButtonBox3 = new HBox();
-
-            TextArea meaning3 = new TextArea(language1);
-            HBox.setMargin(meaning3, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning3,Priority.ALWAYS);
-            meaning3.setPrefHeight(80);
-            meaning3.setPrefWidth(350);
-
-            Button meaning3Button = new Button("<--");
-            HBox.setMargin(meaning3Button, new Insets(25, 10, 0, 0));
-            meaning3Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox3.getChildren().addAll(meaning3, meaning3Button);
-
-            HBox meaningButtonBox4 = new HBox();
-
-            TextArea meaning4 = new TextArea(language1);
-            HBox.setMargin(meaning4, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning4,Priority.ALWAYS);
-            meaning4.setPrefHeight(80);
-            meaning4.setPrefWidth(350);
-
-            Button meaning4Button = new Button("<--");
-            HBox.setMargin(meaning4Button, new Insets(25, 10, 0, 0));
-            meaning4Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox4.getChildren().addAll(meaning4, meaning4Button);
-
-            searchMeaningBox.getChildren().addAll(textWithButton, meaningButtonBox1, meaningButtonBox2,
-                    meaningButtonBox3, meaningButtonBox4);
-        }
-        if (meaningNumber==5) {
-            HBox meaningButtonBox1 = new HBox();
-
-            TextArea meaning1 = new TextArea(language1);
-            HBox.setMargin(meaning1, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning1,Priority.ALWAYS);
-            meaning1.setPrefHeight(80);
-            meaning1.setPrefWidth(350);
-
-            Button meaning1Button = new Button("<--");
-            HBox.setMargin(meaning1Button, new Insets(25, 10, 0, 0));
-            meaning1Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox1.getChildren().addAll(meaning1, meaning1Button);
-
-            HBox meaningButtonBox2 = new HBox();
-
-            TextArea meaning2 = new TextArea(language1);
-            HBox.setMargin(meaning2, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning2,Priority.ALWAYS);
-            meaning2.setPrefHeight(80);
-            meaning2.setPrefWidth(350);
-
-            Button meaning2Button = new Button("<--");
-            HBox.setMargin(meaning2Button, new Insets(25, 10, 0, 0));
-            meaning2Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox2.getChildren().addAll(meaning2, meaning2Button);
-
-            HBox meaningButtonBox3 = new HBox();
-
-            TextArea meaning3 = new TextArea(language1);
-            HBox.setMargin(meaning3, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning3,Priority.ALWAYS);
-            meaning3.setPrefHeight(80);
-            meaning3.setPrefWidth(350);
-
-            Button meaning3Button = new Button("<--");
-            HBox.setMargin(meaning3Button, new Insets(25, 10, 0, 0));
-            meaning3Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox3.getChildren().addAll(meaning3, meaning3Button);
-
-            HBox meaningButtonBox4 = new HBox();
-
-            TextArea meaning4 = new TextArea(language1);
-            HBox.setMargin(meaning4, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning4,Priority.ALWAYS);
-            meaning4.setPrefHeight(80);
-            meaning4.setPrefWidth(350);
-
-            Button meaning4Button = new Button("<--");
-            HBox.setMargin(meaning4Button, new Insets(25, 10, 0, 0));
-            meaning4Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox4.getChildren().addAll(meaning4, meaning4Button);
-
-            HBox meaningButtonBox5 = new HBox();
-
-            TextArea meaning5 = new TextArea(language1);
-            HBox.setMargin(meaning5, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning5,Priority.ALWAYS);
-            meaning5.setPrefHeight(80);
-            meaning5.setPrefWidth(350);
-
-            Button meaning5Button = new Button("<--");
-            HBox.setMargin(meaning5Button, new Insets(25, 10, 0, 0));
-            meaning5Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox5.getChildren().addAll(meaning5, meaning5Button);
-
-            searchMeaningBox.getChildren().addAll(textWithButton, meaningButtonBox1, meaningButtonBox2,
-                    meaningButtonBox3, meaningButtonBox4, meaningButtonBox5);
-        }if (meaningNumber==6) {
-            HBox meaningButtonBox1 = new HBox();
-
-            TextArea meaning1 = new TextArea(language1);
-            HBox.setMargin(meaning1, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning1,Priority.ALWAYS);
-            meaning1.setPrefHeight(80);
-            meaning1.setPrefWidth(350);
-
-            Button meaning1Button = new Button("<--");
-            HBox.setMargin(meaning1Button, new Insets(25, 10, 0, 0));
-            meaning1Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox1.getChildren().addAll(meaning1, meaning1Button);
-
-            HBox meaningButtonBox2 = new HBox();
-
-            TextArea meaning2 = new TextArea(language1);
-            HBox.setMargin(meaning2, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning2,Priority.ALWAYS);
-            meaning2.setPrefHeight(80);
-            meaning2.setPrefWidth(350);
-
-            Button meaning2Button = new Button("<--");
-            HBox.setMargin(meaning2Button, new Insets(25, 10, 0, 0));
-            meaning2Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox2.getChildren().addAll(meaning2, meaning2Button);
-
-            HBox meaningButtonBox3 = new HBox();
-
-            TextArea meaning3 = new TextArea(language1);
-            HBox.setMargin(meaning3, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning3,Priority.ALWAYS);
-            meaning3.setPrefHeight(80);
-            meaning3.setPrefWidth(350);
-
-            Button meaning3Button = new Button("<--");
-            HBox.setMargin(meaning3Button, new Insets(25, 10, 0, 0));
-            meaning3Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox3.getChildren().addAll(meaning3, meaning3Button);
-
-            HBox meaningButtonBox4 = new HBox();
-
-            TextArea meaning4 = new TextArea(language1);
-            HBox.setMargin(meaning4, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning4,Priority.ALWAYS);
-            meaning4.setPrefHeight(80);
-            meaning4.setPrefWidth(350);
-
-            Button meaning4Button = new Button("<--");
-            HBox.setMargin(meaning4Button, new Insets(25, 10, 0, 0));
-            meaning4Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox4.getChildren().addAll(meaning4, meaning4Button);
-
-            HBox meaningButtonBox5 = new HBox();
-
-            TextArea meaning5 = new TextArea(language1);
-            HBox.setMargin(meaning5, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning5,Priority.ALWAYS);
-            meaning5.setPrefHeight(80);
-            meaning5.setPrefWidth(350);
-
-            Button meaning5Button = new Button("<--");
-            HBox.setMargin(meaning5Button, new Insets(25, 10, 0, 0));
-            meaning5Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox5.getChildren().addAll(meaning5, meaning5Button);
-
-            HBox meaningButtonBox6 = new HBox();
-
-            TextArea meaning6 = new TextArea(language1);
-            HBox.setMargin(meaning6, new Insets(0, 20, 0, 60));
-            HBox.setHgrow(meaning6,Priority.ALWAYS);
-            meaning6.setPrefHeight(80);
-            meaning6.setPrefWidth(350);
-
-            Button meaning6Button = new Button("<--");
-            HBox.setMargin(meaning6Button, new Insets(25, 10, 0, 0));
-            meaning6Button.setOnAction(event -> choosingLanguage(textField,stage, scene));
-
-            meaningButtonBox6.getChildren().addAll(meaning6, meaning6Button);
-
-            searchMeaningBox.getChildren().addAll(textWithButton, meaningButtonBox1, meaningButtonBox2,
-                    meaningButtonBox3, meaningButtonBox4, meaningButtonBox5, meaningButtonBox6);
         }
 
 
